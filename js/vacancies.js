d2d1c4a1c1589153951f85cd50fae5a796218e38
artem-2237 <valera8461@gmail.com>
2021-11-08 16:20:38 +0200

    all pages done(non adaptive)

---
+++ js/vacancies.js
@@ -0,0 +1,12 @@
+$('.pagination_1').click(function() {
+    $('.quiz__step').removeClass('quiz__current-step');
+    $('.quiz__first-step').addClass('quiz__current-step');
+})
+$('.pagination_2').click(function() {
+    $('.quiz__step').removeClass('quiz__current-step');
+    $('.quiz__second-step').addClass('quiz__current-step');
+})
+$('.pagination_3').click(function() {
+    $('.quiz__step').removeClass('quiz__current-step');
+    $('.quiz__third-step').addClass('quiz__current-step');
+})
