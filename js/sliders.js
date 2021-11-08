0133c97b86368c4745b01034222d64d5defa4ec3
artem-2237 <valera8461@gmail.com>
2021-11-08 20:49:07 +0200

    itex-agency page done

--- js/sliders.js
+++ js/sliders.js
@@ -40,6 +40,35 @@ $('.expertise-slider').slick({
     ]
 
 });
+$('.partnership-slider').slick({
+    arrows: false,
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 6,
+    adaptiveHeight: true,
+    responsive: [
+        {
+            breakpoint: 1250,
+            settings: {
+                slidesToShow: 4,
+            }
+        },
+        {
+            breakpoint: 968,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 2,
+            }
+        }
+    ]
+
+});
 
 $('.slider-section').slick({
     arrows: false,
@@ -47,8 +76,39 @@ $('.slider-section').slick({
     infinite: true,
     speed: 300,
     slidesToShow: 1,
-     adaptiveHeight: true
+    adaptiveHeight: true
 });
+$('.portfolio-slider').slick({
+    centerMode: true,
+    centerPadding: '230px',
+    focusOnSelect: true,
+    dots: true,
+    slidesToShow: 3,
+    arrows: false,
+    responsive: [
+        {
+            breakpoint: 1250,
+            settings: {
+                slidesToShow: 3,
+                centerPadding: '130px',
+            }
+        },
+        {
+            breakpoint: 968,
+            settings: {
+                slidesToShow: 1,
+                centerPadding: '250px',
+            }
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 1,
+                centerPadding: '0px',
+            }
+        }
+    ]
+})
 
 $('.reviews-slider').slick({
     arrows: false,
